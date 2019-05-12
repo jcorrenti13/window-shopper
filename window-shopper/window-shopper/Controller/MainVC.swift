ef3407d86bda99103001a4e13f7aecf3fb616e05
Jake Correnti <jakecorrenti@Jakes-MacBook-Pro.local>
2019-05-12 10:39:01 -0400

    Finsihed app

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageText: CurrencyTextField!
+    @IBOutlet weak var itemPrice: CurrencyTextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageText.inputAccessoryView = calcButton
+        itemPrice.inputAccessoryView = calcButton
+        
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+        
     }
-
+    
+    @objc func calculate() {
+        // if wageText and itemPrice are not nil, assign the value to the constants
+        if let wageText = wageText.text, let itemPrice = itemPrice.text {
+            // takes the values of the constants above and casts them into doubles if they are numbers
+            if let wage = Double(wageText), let price = Double(itemPrice) {
+                view.endEditing(true)
+                resultLabel.isHidden = false
+                hoursLabel.isHidden = false
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+                
+            }
+        }
+    }
+    
+    @IBAction func clearCalculatorButtonPressed(_ sender: Any) {
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+        wageText.text = ""
+        itemPrice.text = ""
+    }
 }
 
--- window-shopper/window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
+@IBDesignable 
 class CurrencyTextField: UITextField {
     
+    // never keep empty
+    // never call super
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let  currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2 - size / 2), width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     // essentially view did load for view elements??? do research on awakeFromNib()
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        customizeView()
+    }
+    
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
-        layer.cornerRadius = 5.0
+        layer.cornerRadius = 10.0
         textAlignment = .center
-        
-        
+        clipsToBounds = true
         if let p = placeholder {
             let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)])
             attributedPlaceholder = place
